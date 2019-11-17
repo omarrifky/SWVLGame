233dc8062521202716e6c28ebe11ab888029bb0b
mash197 <mohamedashraf8997@gmail.com>
2019-11-17 18:19:12 +0100

    modified the bus stop shape

--- Assets/Scripts/BusMovement.cs
+++ Assets/Scripts/BusMovement.cs
@@ -14,23 +14,18 @@ public class BusMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z >= 650 && transform.position.x >= 3.6)
-        {
+        Debug.Log((int)Time.time);
+        if(transform.position.z >= 650 && transform.position.x >= 3.6){
             busarrived = true;
         }
-        float horizontal = Input.GetAxis("Horizontal");
-        if(transform.position.x<=3.640127&& horizontal>0)
-        transform.Translate(5 * horizontal * Time.deltaTime, 0,0);  
-        if (transform.position.x >= -3.560907 && horizontal < 0)
-        transform.Translate(8 * horizontal * Time.deltaTime, 0, 0);
 
+        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        if(transform.position.z <= 666 && vertical > 0)
-        transform.Translate(0, 0, 10* vertical * Time.deltaTime);
-
-        if (transform.position.z >= -11.6 && vertical < 0)
-            transform.Translate(0, 0, 8 * vertical * Time.deltaTime);
-
+        if((transform.position.z <= 666 && vertical > 0) || (transform.position.z >= -11.6 && vertical < 0)){
+            transform.Translate(0, 0, 10* vertical * Time.deltaTime);
+            if((transform.position.x<=3.640127&& horizontal>0) || (transform.position.x >= -3.560907 && horizontal < 0))
+            transform.Translate(5 * horizontal * Time.deltaTime, 0,0);  
+        }
     }
 }
