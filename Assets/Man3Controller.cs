d9fcbb22c83f1d8cf95ca3e9cbb5584cdd581711
mash197 <mohamedashraf8997@gmail.com>
2019-11-20 18:14:27 +0100

    merge

---
+++ Assets/Man3Controller.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Man3Controller : MonoBehaviour
+{
+    Animator anim;
+   public AudioSource cashsound;
+   public AudioSource latebuzzer;
+    bool waving = false;
+    bool getonbus = false;
+    public GameObject Bus;
+    public GameObject station1;
+    public GameObject station2;
+    public GameObject station3;
+    public GameObject station4;
+    bool late = false;
+    bool destinationlate = false;
+    bool busarrived = false;
+     float animover;
+     bool onetime = false;
+
+    bool once = false;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        busarrived = GameObject.Find("Bus").GetComponent<BusMovement>().busarrived;
+
+        busarrived = GameObject.Find("Bus").GetComponent<BusMovement>().busarrived;
+         if(busarrived==true && Time.time <= 89&& once == false){
+            destinationlate = false;
+            once = true;
+            print("ARRIVED");
+        }
+
+        if(busarrived==true && Time.time > 89 && once == false){
+            destinationlate = true;
+            once = true;
+        }
+
+        if (station1.transform.position.z <= Bus.transform.position.z + 2 && station1.transform.position.z + 5 >= Bus.transform.position.z && Bus.transform.position.x >= 3.6 && Time.time < 57)
+        {
+            late = false;
+        }
+
+        if (station1.transform.position.z <= Bus.transform.position.z + 2 && station1.transform.position.z + 5 >= Bus.transform.position.z && Bus.transform.position.x >= 3.6 && Time.time >= 57)
+        {
+            late = true;
+        }
+
+        
+
+        if (station1.transform.position.z <= Bus.transform.position.z + 2 && station1.transform.position.z + 5 >= Bus.transform.position.z && Bus.transform.position.x >= 3.6 && getonbus == false && late == false)
+        {
+            waving = true;
+            anim.SetBool("StartWaving", waving);
+            getonbus = true;
+            anim.SetBool("GetOnBus", getonbus);
+           
+            cashsound.Play();
+        }
+
+        if (station1.transform.position.z <= Bus.transform.position.z + 2 && station1.transform.position.z + 5 >= Bus.transform.position.z && Bus.transform.position.x >= 3.6 && getonbus == false && late == true)
+        {
+            anim.SetBool("Late", late);
+            getonbus = true;
+            anim.SetBool("GetOnBus", getonbus);
+            latebuzzer.Play();
+        }
+
+         if(getonbus==true){
+         if(onetime == false){
+            if(late==false)
+            animover = Time.time + 2f;
+            else
+             animover = Time.time + 9f;
+            onetime = true;
+            
+            }
+            if(Time.time > animover&& onetime == true){
+            
+                transform.position = new Vector3(500f, 0.42f, 666);
+                onetime = false;
+            }
+        }
+
+        if(busarrived ==true && destinationlate == false)
+        {
+            
+            anim.SetBool("StartDancing", busarrived);
+             transform.position = new Vector3(6.66f, 0.42f, 669.41f);
+        }
+
+        if (busarrived == true && destinationlate == true)
+        {
+            anim.SetBool("StartYelling", destinationlate);
+             transform.position = new Vector3(6.66f, 0.42f, 669.41f);
+        }
+    }
+}
